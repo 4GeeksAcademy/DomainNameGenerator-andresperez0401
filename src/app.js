523210957c9b2dea802eff4db8f80cac9ff2e53a
Andres Perez <andresperez0401@gmail.com>
2025-01-16 15:50:53 -0400

    Domain Generator completed

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extension = ['.com', '.net'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let resultado = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function domainNameGenerator(domainNames){
+
+  //Ciclos anidados para recorrer cada uno de los arreglos y realizar las combinaciones
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+        for (let n = 0; n < noun.length; n++) {
+            for (let e = 0; e < extension.length; e++) {
+                resultado.push(pronoun[p] + adj[a] + noun[n] + extension[e]);
+            }
+        }
+    }
+  }   
+  return resultado;
+}
+
+console.log(domainNameGenerator(resultado));
